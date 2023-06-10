f6cfca9ec65a76af6b38b56a90b2911f42e22c09
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-06-09 22:46:03 -0300

    enumerations-composition

---
+++ Supermarket/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using Supermarket.Entities;
+using Supermarket.Entities.Enums;
+
+namespace Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data:");
+            DateTime moment = DateTime.Now;
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Order order = new Order(moment, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                Product product = new Product(productName, productPrice);
+                OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);      
+        }
+    }
+}
---
+++ WorkersControl/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkersControl.Entities.Enums
+{
+    internal enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
